2c7f3e7aefbbbbd572a4a86d148a81213479fda7
Bruce Hem <bruce.hem@pathmazing.com>
2017-08-17 16:43:20 +0700

    fix?

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-const { NODE_ENV = 'development', PORT = 5000, DB_URI = '' } = process.env // es6 destructuring
+const {
+  NODE_ENV = 'development',
+  PORT = 5000,
+  DB_URI = 'mysql://b7s5u0xkwiyxsv6w:jddl4j3isvqbf3j1@cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u9dp4n7omoxst4ed'
+} = process.env // es6 destructuring
 
 exports.app = {
   env: NODE_ENV,
