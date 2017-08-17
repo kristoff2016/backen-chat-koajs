97501e5c9b6f6cd115f9f3e252b7cb71974cfa7e
Bruce Hem <bruce.hem@pathmazing.com>
2017-08-17 16:45:16 +0700

    update config

--- config.js
+++ config.js
@@ -1,14 +1,8 @@
-const {
-  NODE_ENV = 'development',
-  PORT = 5000,
-  DB_URI = 'mysql://b7s5u0xkwiyxsv6w:jddl4j3isvqbf3j1@cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u9dp4n7omoxst4ed'
-} = process.env // es6 destructuring
-
 exports.app = {
-  env: NODE_ENV,
-  port: PORT
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 5000
 }
 
 exports.db = {
-  uri: DB_URI
+  uri: process.env.DB_URI || ''
 }
