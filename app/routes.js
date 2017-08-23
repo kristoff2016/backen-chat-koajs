4d0e153d767b44bbe9b6135948cf2ab3e909ba01
Thor Mengkheang <thormengkheang@gmail.com>
2017-08-23 10:00:19 +0700

    add chat message route

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,10 @@
 const WelcomeController = require('./controllers/welcome.controller')
 const UserController = require('./controllers/user.controller')
-const ChatController = require('./controllers/chat.controller')
+// const ChatController = require('./controllers/chat.controller')
+const ChatMessageController = require('./controllers/chat-message.controller')
 const GeneralController = require('./controllers/upload.controller')
 const { validateEmail, validateLogin } = require('./middleware/user.middleware')
+const { validateContent, validateChat, validateChatMessage } = require('./middleware/chat-message.middleware')
 const { isMultiPart, singleUpload, singleVideoUpload } = require('./middleware/upload.middleware')
 
 const { isAuthenticated } = require('./middleware/auth.middleware')
@@ -11,15 +13,15 @@ module.exports = [
     prefix: '/',
     routes: [ { method: 'GET', path: '/', middleware: [], handler: WelcomeController.welcome } ]
   },
-  {
-    prefix: '/v1/chats',
-    routes: [
-      { method: 'GET', path: '/', middleware: [], handler: UserController.listUser },
-      { method: 'POST', path: '/', middleware: [ isAuthenticated ], handler: ChatController.createChat },
-      { method: 'POST', path: '/:id/invites', middleware: [ isAuthenticated ], handler: ChatController.inviteUser },
-      { method: 'POST', path: '/:id/kicks', middleware: [ isAuthenticated ], handler: ChatController.kickUser }
-    ]
-  },
+  // {
+  //   prefix: '/v1/chats',
+  //   routes: [
+  //     { method: 'GET', path: '/', middleware: [], handler: UserController.listUser },
+  //     { method: 'POST', path: '/', middleware: [ isAuthenticated ], handler: ChatController.createChat },
+  //     { method: 'POST', path: '/:id/invites', middleware: [ isAuthenticated ], handler: ChatController.inviteUser },
+  //     { method: 'POST', path: '/:id/kicks', middleware: [ isAuthenticated ], handler: ChatController.kickUser }
+  //   ]
+  // },
 
   // create chat (w/ invited users[])
   // invite users <--
@@ -29,7 +31,29 @@ module.exports = [
   // send/create chat message (API/ realtime)
   // edit
   // delete
-
+  {
+    prefix: '/v1/chats',
+    routes: [
+      {
+        method: 'POST',
+        path: '/:id/messages',
+        middleware: [ isAuthenticated, validateContent, validateChat ],
+        handler: ChatMessageController.sendMessage
+      },
+      {
+        method: 'PUT',
+        path: '/:id/message/:messageId', // DEL /v1/chat/1/messages/10
+        middleware: [ isAuthenticated, validateContent, validateChat, validateChatMessage ],
+        handler: ChatMessageController.editMessage
+      },
+      {
+        method: 'DELETE',
+        path: '/:id/message/:messageId',
+        middleware: [ isAuthenticated, validateChat, validateChatMessage ],
+        handler: ChatMessageController.deleteMessage
+      }
+    ]
+  },
   {
     prefix: '/v1/upload',
     routes: [
