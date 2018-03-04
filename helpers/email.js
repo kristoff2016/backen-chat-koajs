7ab5ea445dd6b311d9ef7faab61e32670193be35
Lyheang IBell <kristoff2016@users.noreply.github.com>
2018-03-04 19:28:12 +0700

    Update email.js

--- helpers/email.js
+++ helpers/email.js
@@ -1,18 +1,1 @@
-const nodemailer = require('nodemailer')
-const transport = nodemailer.createTransport(
-  {
-    service: 'Gmail',
-    auth: {
-      user: 'brainmusic2017@gmail.com',
-      pass: '098355644'
-    },
-    debug: true
-  },
-  {
-    from: 'Brain Music brainmusic2017@gmail.com',
-    headers: {
-      'X-Laziness-level': 1000
-    }
-  }
-)
-module.exports = { transport }
+
